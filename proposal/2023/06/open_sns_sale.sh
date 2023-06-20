cfe554796f181c24d8ac691def7841dd116ef27a
utkgoyal <89467029+utkgoyal@users.noreply.github.com>
2023-06-20 10:30:12 -0700

    Update open_sns_sale.sh
    The values have been updated. 2 values remain - target swap canister id and summary

--- proposal/2023/06/open_sns_sale.sh
+++ proposal/2023/06/open_sns_sale.sh
@@ -2,16 +2,16 @@ sns deploy --network "ic" --init-config-file "./sns/sns_init.yaml" --save-to "sn
 
 ic-admin   \
    --nns-url "https://nns.ic0.app" propose-to-open-sns-token-swap  \
-   --min-participants 1  \
+   --min-participants 500  \
    --min-icp-e8s 100000000000000  \
    --max-icp-e8s 200000000000000  \
    --min-participant-icp-e8s 100000000  \
    --max-participant-icp-e8s 15000000000000  \
-   --swap-due-timestamp-seconds "1296000"  \
+   --swap-due-timestamp-seconds "1689148800"  \
    --sns-token-e8s 33000000000000000  \
    --target-swap-canister-id "${SNS_SWAP_ID}"  \
-   --community-fund-investment-e8s 501_000_00_000_000  \
+   --community-fund-investment-e8s 50100000000000  \
    --neuron-basket-count 5  \
    --neuron-basket-dissolve-delay-interval-seconds 15778800  \
-   --proposal-title "Decentralize this SNS"  \
-   --summary "Decentralize this SNS"
+   --proposal-title "Proposal to create an SNS-DAO for Hot or Not"  \
+   --summary "Decentralize this SNS" #Edit this from the Notion file
