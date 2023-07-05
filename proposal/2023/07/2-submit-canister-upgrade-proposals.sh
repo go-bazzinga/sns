f96596023e905eb32c15f680a65ccc28287d566d
Saikat Das <saikatdas0790@gmail.com>
2023-07-05 16:07:34 +0200

    chore: save fleet upgrade SNS proposal quill command

--- proposal/2023/07/2-submit-canister-upgrade-proposals.sh
+++ proposal/2023/07/2-submit-canister-upgrade-proposals.sh
@@ -15,47 +15,53 @@ dfx build --network=ic post_cache
 export CANISTER_IDS_FILE="./sns/sns_canister_ids.json"
 export APP_URL="https://hotornot.wtf/"
 
-export CANISTER_NAME="configuration"
-./quill sns \
-  --canister-ids-file "${CANISTER_IDS_FILE}"  \
-  --pem-file "${PEM_FILE}"  \
-  make-upgrade-canister-proposal \
-  --title "Upgrade ${CANISTER_NAME} canister"  \
-  --summary "This proposal upgrades ${CANISTER_NAME} canister"  \
-  --url "${APP_URL}"  \
-  --target-canister-id "$(dfx canister id ${CANISTER_NAME} --network ic)" \
-  --wasm-path "./.dfx/ic/canisters/${CANISTER_NAME}/${CANISTER_NAME}.wasm.gz" \
-  --canister-upgrade-arg "(record {})" \
-"54f9ba2b0e81a17f5261b277abd91816e041c5ca749ba88b3dbe05f66bb6124d" > "./sns/proposal/2023/07/upgrade_${CANISTER_NAME}_canister_message.json"
-./quill send "./sns/proposal/2023/07/upgrade_${CANISTER_NAME}_canister_message.json" --yes
+# export CANISTER_NAME="configuration"
+# ./quill sns \
+#   --canister-ids-file "${CANISTER_IDS_FILE}"  \
+#   --pem-file "${PEM_FILE}"  \
+#   make-upgrade-canister-proposal \
+#   --title "Upgrade ${CANISTER_NAME} canister"  \
+#   --summary "This proposal upgrades ${CANISTER_NAME} canister"  \
+#   --url "${APP_URL}"  \
+#   --target-canister-id "$(dfx canister id ${CANISTER_NAME} --network ic)" \
+#   --wasm-path "./.dfx/ic/canisters/${CANISTER_NAME}/${CANISTER_NAME}.wasm.gz" \
+#   --canister-upgrade-arg "(record {})" \
+# "54f9ba2b0e81a17f5261b277abd91816e041c5ca749ba88b3dbe05f66bb6124d" > "./sns/proposal/2023/07/upgrade_${CANISTER_NAME}_canister_message.json"
+# ./quill send "./sns/proposal/2023/07/upgrade_${CANISTER_NAME}_canister_message.json" --yes
 
-export CANISTER_NAME="data_backup"
-./quill sns \
-  --canister-ids-file "${CANISTER_IDS_FILE}"  \
-  --pem-file "${PEM_FILE}"  \
-  make-upgrade-canister-proposal \
-  --title "Upgrade ${CANISTER_NAME} canister"  \
-  --summary "This proposal upgrades ${CANISTER_NAME} canister"  \
-  --url "${APP_URL}"  \
-  --target-canister-id "$(dfx canister id ${CANISTER_NAME} --network ic)" \
-  --wasm-path "./.dfx/ic/canisters/${CANISTER_NAME}/${CANISTER_NAME}.wasm.gz" \
-  --canister-upgrade-arg "(record {})" \
-"54f9ba2b0e81a17f5261b277abd91816e041c5ca749ba88b3dbe05f66bb6124d" > "./sns/proposal/2023/07/upgrade_${CANISTER_NAME}_canister_message.json"
-./quill send "./sns/proposal/2023/07/upgrade_${CANISTER_NAME}_canister_message.json" --yes
+# sleep 300
 
-export CANISTER_NAME="post_cache"
-./quill sns \
-  --canister-ids-file "${CANISTER_IDS_FILE}"  \
-  --pem-file "${PEM_FILE}"  \
-  make-upgrade-canister-proposal \
-  --title "Upgrade ${CANISTER_NAME} canister"  \
-  --summary "This proposal upgrades ${CANISTER_NAME} canister"  \
-  --url "${APP_URL}"  \
-  --target-canister-id "$(dfx canister id ${CANISTER_NAME} --network ic)" \
-  --wasm-path "./.dfx/ic/canisters/${CANISTER_NAME}/${CANISTER_NAME}.wasm.gz" \
-  --canister-upgrade-arg "(record {})" \
-"54f9ba2b0e81a17f5261b277abd91816e041c5ca749ba88b3dbe05f66bb6124d" > "./sns/proposal/2023/07/upgrade_${CANISTER_NAME}_canister_message.json"
-./quill send "./sns/proposal/2023/07/upgrade_${CANISTER_NAME}_canister_message.json" --yes
+# export CANISTER_NAME="data_backup"
+# ./quill sns \
+#   --canister-ids-file "${CANISTER_IDS_FILE}"  \
+#   --pem-file "${PEM_FILE}"  \
+#   make-upgrade-canister-proposal \
+#   --title "Upgrade ${CANISTER_NAME} canister"  \
+#   --summary "This proposal upgrades ${CANISTER_NAME} canister"  \
+#   --url "${APP_URL}"  \
+#   --target-canister-id "$(dfx canister id ${CANISTER_NAME} --network ic)" \
+#   --wasm-path "./.dfx/ic/canisters/${CANISTER_NAME}/${CANISTER_NAME}.wasm.gz" \
+#   --canister-upgrade-arg "(record {})" \
+# "54f9ba2b0e81a17f5261b277abd91816e041c5ca749ba88b3dbe05f66bb6124d" > "./sns/proposal/2023/07/upgrade_${CANISTER_NAME}_canister_message.json"
+# ./quill send "./sns/proposal/2023/07/upgrade_${CANISTER_NAME}_canister_message.json" --yes
+
+# sleep 300
+
+# export CANISTER_NAME="post_cache"
+# ./quill sns \
+#   --canister-ids-file "${CANISTER_IDS_FILE}"  \
+#   --pem-file "${PEM_FILE}"  \
+#   make-upgrade-canister-proposal \
+#   --title "Upgrade ${CANISTER_NAME} canister"  \
+#   --summary "This proposal upgrades ${CANISTER_NAME} canister"  \
+#   --url "${APP_URL}"  \
+#   --target-canister-id "$(dfx canister id ${CANISTER_NAME} --network ic)" \
+#   --wasm-path "./.dfx/ic/canisters/${CANISTER_NAME}/${CANISTER_NAME}.wasm.gz" \
+#   --canister-upgrade-arg "(record {})" \
+# "54f9ba2b0e81a17f5261b277abd91816e041c5ca749ba88b3dbe05f66bb6124d" > "./sns/proposal/2023/07/upgrade_${CANISTER_NAME}_canister_message.json"
+# ./quill send "./sns/proposal/2023/07/upgrade_${CANISTER_NAME}_canister_message.json" --yes
+
+# sleep 300
 
 export CANISTER_NAME="user_index"
 ./quill sns \
