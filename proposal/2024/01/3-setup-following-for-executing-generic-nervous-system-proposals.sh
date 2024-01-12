892700c669a08ff45c768cba85be9fe544efe0b5
Saikat Das <saikatdas0790@gmail.com>
2024-01-12 17:44:59 +0530

    chore: setup following for generic nervous system function

---
+++ proposal/2024/01/3-setup-following-for-executing-generic-nervous-system-proposals.sh
@@ -0,0 +1,18 @@
+set -euo pipefail
+
+export IDENTITY_NAME="hot_or_not_sns"
+export PEM_FILE="$(readlink -f ~/.config/dfx/identity/${IDENTITY_NAME}/identity.pem)"
+export CANISTER_IDS_FILE="./sns/sns_canister_ids.json"
+export NEURON_ID="54f9ba2b0e81a17f5261b277abd91816e041c5ca749ba88b3dbe05f66bb6124d"
+
+dfx identity use "${IDENTITY_NAME}"
+
+./quill sns \
+  --canister-ids-file "${CANISTER_IDS_FILE}"  \
+  --pem-file "${PEM_FILE}"  \
+  follow-neuron \
+  --followees a133890831635bad4b955327fba37d91759fa1d1c4971503cf4670ea630e9a18 \
+  --function-id 4001 \
+  ${NEURON_ID} > "./sns/proposal/2024/01/3-setup-following-for-executing-generic-nervous-system-proposals.json"
+
+./quill send "./sns/proposal/2024/01/3-setup-following-for-executing-generic-nervous-system-proposals.json" --yes
