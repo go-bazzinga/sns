7b6f7613520b8f903bb6e8538b5d80a37dbc6dbf
Saikat Das <saikatdas0790@gmail.com>
2024-02-12 17:13:45 +0530

    chore: vote on proposal

--- proposal/2024/01/2-vote-on-proposal.sh
+++ proposal/2024/01/2-vote-on-proposal.sh
@@ -4,7 +4,7 @@ export IDENTITY_NAME="hot_or_not_sns"
 export PEM_FILE="$(readlink -f ~/.config/dfx/identity/${IDENTITY_NAME}/identity.pem)"
 dfx identity use "${IDENTITY_NAME}"
 export CANISTER_IDS_FILE="./sns/sns_canister_ids.json"
-export PROPOSAL_ID="65"
+export PROPOSAL_ID="78"
 export VOTE="y"
 export NEURON_ID="54f9ba2b0e81a17f5261b277abd91816e041c5ca749ba88b3dbe05f66bb6124d"
 
